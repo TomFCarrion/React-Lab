7d97e7ce52430b6db1b7182509bf940bdf17608c
Tomas F. Carrion <tomfcarrion@gmail.com>
2019-11-14 19:54:45 -0300

    add photoCard component

--- src/components/photoCard/index.js
+++ src/components/photoCard/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {ImgWrapper, Img, Button} from 'react';
+import { ImgWrapper, Img, Button } from './styles';
+import { MdFavoriteBorder } from 'react-icons/md';
 
+const DEFAULT_IMAGE = 'https://images.pexels.com/photos/931018/pexels-photo-931018.jpeg'
 
-export const PhotoCard = ({id, likes = 0, src}) => {
-    return(
-        <article>
-            <a href={`/detail/${id}`}>
-                <ImgWrapper>
-                    <Img src={src}/>
-                </ImgWrapper>
-            </a>
-            <Button>
-            {likes} likes!
-            </Button>
-        </article>
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  return (
+    <article>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
 
-    )
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} likes!
+      </Button>
+    </article>
+  )
 }
