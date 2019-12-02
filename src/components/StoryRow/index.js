6e1c54ee71cc0ab16b099f4f0196baf4909490ef
Tomas F. Carrion <tomfcarrion@gmail.com>
2019-12-02 17:12:54 -0300

    feat/api: add pexels fetch

--- src/components/StoryRow/index.js
+++ src/components/StoryRow/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { Story } from '../Story/index';
 import {List, Item } from './styles'
 import { mockUsers} from '../../../api/db.json'
 
 export const StoryRow = () => {
+
+    const [hasError, setErrors] = useState(false);
+    const [photos, setPhotos] = useState([]);
+
+    const UsersFetch = () => {
+        async function fetchData() {
+          const res = await fetch('https://api.pexels.com/v1/search?query=people+query&per_page=15&page=1', { 
+            method: 'GET', 
+            headers: new Headers({
+              'Authorization': '563492ad6f91700001000001b68f03e4b49c4fa19d3267e8a5dc7c15'
+            })
+          });
+            res.json()
+            .then(res => setPhotos(res.photos))
+            .catch(err => setErrors(err))
+        }
+
+        useEffect(() => {
+            fetchData();
+        }, [])
+
+        console.log(photos);
+
+    
+        return photos;
+    }
+
+    UsersFetch();
+
     return (
         <List>
             {
-             mockUsers.map(story => <Item key={story.id}><Story {...story}/></Item>)
+             photos.map(story => <Item key={story.id}><Story cover= { ((story||{}).src || {}).small} userName={(story||{}).photographer}/></Item>)
             }
         </List>
     )
--- src/components/photoCardList/index.js
+++ src/components/photoCardList/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { PhotoCard } from '../photoCard/index';
 
 export const PhotoCardList = () => {
+
+    const [hasError, setErrors] = useState(false);
+    const [photos, setPhotos] = useState([]);
+
+    const photoCardFetch = () => {
+        async function fetchData() {
+          const res = await fetch('https://api.pexels.com/v1/search?query=friends+query&per_page=15&page=1', { 
+            method: 'GET', 
+            headers: new Headers({
+              'Authorization': '563492ad6f91700001000001b68f03e4b49c4fa19d3267e8a5dc7c15'
+            })
+          });
+            res.json()
+            .then(res => setPhotos(res.photos))
+            .catch(err => setErrors(err))
+        }
+
+        useEffect(() => {
+            fetchData();
+        }, [])
+
+        console.log(photos);
+
+    
+        return photos;
+    }
+
+    photoCardFetch();
+
     return (
         <ul>
-            {[1,2,3,4,5].map(id => <PhotoCard key={id}/> )}
+            {photos.map(photo => <PhotoCard src={((photo||{}).src || {}).medium} key={photo.id}/> )}
         </ul>
     )
 }
